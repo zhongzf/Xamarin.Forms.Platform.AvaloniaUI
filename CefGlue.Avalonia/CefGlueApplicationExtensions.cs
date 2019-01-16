232f020564b85f0ee3e8ce8a7b5b8143ac697f95
johnson.zhong <johnson.zhong@innocellence.com>
2019-01-16 13:46:36 +0800

    feat: include the CefGlue.Core project, the sample can work.

---
+++ CefGlue.Avalonia/CefGlueApplicationExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia.Controls;
+using System;
+using Xilium.CefGlue;
+
+namespace CefGlue.Avalonia
+{
+    public static class CefGlueApplicationExtensions
+    {
+        public static T ConfigureCefGlue<T>(this T builder, string[] args) where T : AppBuilderBase<T>, new()
+        {
+            return builder.AfterSetup((b) =>
+            {
+                try
+                {
+                    CefRuntime.Load();
+                }
+                catch (DllNotFoundException ex)
+                {
+
+                }
+                catch (CefRuntimeException ex)
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+                var mainArgs = new CefMainArgs(args);
+                var cefApp = new SampleCefApp();
+
+                var exitCode = CefRuntime.ExecuteProcess(mainArgs, cefApp);
+                if (exitCode != -1) { return; }
+
+                var location = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var directory = System.IO.Path.GetDirectoryName(location);
+
+                var cefSettings = new CefSettings
+                {
+                    SingleProcess = true,
+                    WindowlessRenderingEnabled = true,
+                    MultiThreadedMessageLoop = false,
+                    LogSeverity = CefLogSeverity.Disable,
+                    LogFile = "cef.log",
+                    ExternalMessagePump = true
+                };
+
+                try
+                {
+                    CefRuntime.Initialize(mainArgs, cefSettings, cefApp);
+                }
+                catch (CefRuntimeException ex)
+                {
+
+                }
+            });
+        }
+    }
+}
