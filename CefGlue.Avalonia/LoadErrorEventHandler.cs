4dd1ccf673670624b3564fed760fce8ddf3b7eb1
zhongzf <zhongzf@126.com>
2019-01-16 20:35:33 +0800

    feat: upgrade to CefGlue.Core 0.9.0-build32-alpha.

--- CefGlue.Avalonia/LoadErrorEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CefGlue.Avalonia
-{
-    public delegate void LoadErrorEventHandler(object sender, LoadErrorEventArgs e);
-}
--- CefGlue.Avalonia/LoadingStateChangeEventHandler.cs
+++
@@ -1,5 +0,0 @@
-﻿
-namespace CefGlue.Avalonia
-{
-    public delegate void LoadingStateChangeEventHandler(object sender, LoadingStateChangeEventArgs e);
-}
